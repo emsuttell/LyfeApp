00c5102477375ad0c0b6d7eaa0562154fe0e2605
manasa <lgmanasa@gmail.com>
2020-08-12 16:15:03 -0700

    to do lists

---
+++ LyfeApp/ToDO.swift
@@ -0,0 +1,14 @@
+//
+//  ToDO.swift
+//  LyfeApp
+//
+//  Created by Apple on 8/12/20.
+//  Copyright © 2020 EmmaxManasaCode. All rights reserved.
+//
+
+import UIKit
+
+class ToDo{
+    var hours = ""
+    var enoughSleep = false
+}
