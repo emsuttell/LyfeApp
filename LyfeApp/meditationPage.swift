a8121a7ac38dd2c800d24ed03ada87234c18482d
Emma Suttell <emsuttell@gmail.com>
2020-08-13 12:06:45 -0700

    added meditation buttons

--- LyfeApp/meditationPage.swift
+++ LyfeApp/meditationPage.swift
@@ -11,15 +11,23 @@ import AVFoundation
 
 class meditationPage: UIViewController {
     var audioPlayer = AVAudioPlayer()
+    var audioPlayer1 = AVAudioPlayer()
+    var audioPlayer2 = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     let sound = Bundle.main.path(forResource: "bensoundrelaxing", ofType: "mp3")
+    let sound1 = Bundle.main.path(forResource: "gamela", ofType: "mp3")
+     let sound2 = Bundle.main.path(forResource: "dreamescape", ofType: "mp3")
 
     do{
         
     audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        
+    audioPlayer1 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound1!))
+        
+    audioPlayer2 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound2!))
     }
     catch{
             print(error)
@@ -36,10 +44,29 @@ class meditationPage: UIViewController {
         audioPlayer.stop()
     }
 
+    @IBAction func playButtonGamela(_ sender: Any) {
+        audioPlayer1.play()
+        
+    }
+    
+    @IBAction func stopButtonGamela(_ sender: Any) {
+        audioPlayer1.stop()
+    }
     
     
+    @IBAction func playButtonDream(_ sender: Any) {
+        audioPlayer2.play()
+        
+    }
+    
+    @IBAction func stopButtonDream(_ sender: Any) {
+        audioPlayer2.stop()
+    }
     
     
+    @IBAction func breatheButton(_ sender: Any) {
+        if let url = NSURL(string: "https://www.helpguide.org/meditations/deep-breathing-meditation.htm"){             UIApplication.shared.open(url as URL) }
+    }
     
     /*
     // MARK: - Navigation
