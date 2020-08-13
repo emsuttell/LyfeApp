235d50efdda6bc8a0a12aab0a4155087c67fa64a
manasa <lgmanasa@gmail.com>
2020-08-12 20:47:15 -0700

    to-do list + view controller to add to the to list (added color)

--- LyfeApp/ToDoTableViewController.swift
+++ LyfeApp/ToDoTableViewController.swift
@@ -9,46 +9,44 @@
 import UIKit
 
 class ToDoTableViewController: UITableViewController {
-    
-    var toDos : [ToDo] = []
-    
-    func createToDos() -> [ToDo] {
-
-      let swift = ToDo()
-      swift.hours = "Learn Swift"
-      swift.enoughSleep = true
-
-      let dog = ToDo()
-      dog.hours = "Walk the Dog"
-      // important is set to false by default
-
-      return [swift, dog]
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-         toDos = createToDos()
         
+         toDos = createToDos()
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    var toDos : [ToDo] = []
+    
+    func createToDos() -> [ToDo] {
+
+      let swift = ToDo()
+      swift.name = "Learn Swift"
+      swift.important = true
+
+      let dog = ToDo()
+      dog.name = "Walk the Dog"
+      // important is set to false by default
+
+      return [swift, dog]
+    }
 
     // MARK: - Table view data source
 
-/*    override func numberOfSections(in tableView: UITableView) -> Int {
+   /* override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
- 
- */
-
+*/
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-          return toDos.count
+         return toDos.count
     }
 
     
@@ -56,12 +54,11 @@ class ToDoTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
        let toDo = toDos[indexPath.row]
-
-        if toDo.enoughSleep {
-          cell.textLabel?.text = "❗️" + toDo.hours
-        } else {
-          cell.textLabel?.text = toDo.hours
-        }
+        if toDo.important {
+           cell.textLabel?.text = "❗️" + toDo.name
+         } else {
+           cell.textLabel?.text = toDo.name
+         }
 
         return cell
     }
@@ -102,14 +99,19 @@ class ToDoTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let addVC = segue.destination as? AddToDoViewController {
+          addVC.previousVC = self
+        }
+        
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
